7e7a980824c80945b4cebbe00462f90b9700244a
Assar <33634310+assarbertil@users.noreply.github.com>
2022-08-29 14:30:52 +0200

    added different urls to websocket depending on env

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client"
 
-export const socket = io("ws://localhost:3001", {
-  autoConnect: false,
-})
+export const socket = io(
+  process.env.NODE_ENV === "development"
+    ? "ws://localhost:3001"
+    : "wss://gridpainter.onrender.com",
+  { autoConnect: false }
+)
 
 socket.onAny((event, ...args) => {
   console.log(`Tog emot event ${event}`, args)
@@ -12,6 +15,6 @@ socket.on("userInfo", (team, userName) => {
   console.log("Loggade in i lag:", team, " med användarnamn:", userName)
 })
 
-socket.on("roomData", (users) => {
+socket.on("roomData", users => {
   console.log(users)
 })
